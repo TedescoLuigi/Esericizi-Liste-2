ecf3c056e821d556f8dc2289f6fb37d91ae1e111
tedesco.luigi <tedesco.luigi@INF-1010>
2026-04-13 08:51:49 +0200

    Aggiungere i file di progetto.

---
+++ Esericizi Liste 2/Program.cs
@@ -0,0 +1,47 @@
+﻿namespace Esericizi_Liste_2
+{
+    internal class Program
+    {
+        //indice esplosivo 
+
+        static void Esplosivo (List<int> valori )
+        {
+            for (int i=  valori.Count-1;i>=0;i--)
+            {
+              if (valori[i] == i)
+              {
+                    valori.Remove(i);
+              }
+            }
+            foreach (int i in valori)
+            {
+                Console.WriteLine("["+i+"]");
+
+            }
+        }
+
+        //Rotazione della Lista
+        static void Rotazione(List<int> numeri1)
+        {
+            Console.WriteLine("inserisci un numero che spostera la lista di tot valore");
+            int scelta=Convert.ToInt32 (Console.ReadLine());
+
+            for (int i = 0; i < numeri1.Count; i++)
+            { 
+                
+
+            }
+
+        }
+        static void Main(string[] args)
+        {
+            //indice esplosivo 
+            List<int> valori = new List<int>() { 0, 5, 2, 8, 4, 10, 6, 7 };
+            Esplosivo(valori);
+            Console.WriteLine(valori);
+
+            //Rotazione della Lista
+            List<int> numeri1 = new List<int>() { 1, 2, 3, 4, 5 };
+        }
+    }
+}
