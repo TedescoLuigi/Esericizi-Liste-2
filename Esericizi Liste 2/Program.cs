045c7dc67304daf2374bda46abd8873079b6030a
TedescoLuigi <tedesco.luigi@franchettisalviani.net>
2026-04-14 10:53:24 +0200

    11

--- Esericizi Liste 2/Program.cs
+++ Esericizi Liste 2/Program.cs
@@ -4,46 +4,71 @@
     {
         //indice esplosivo 
 
-        static void Esplosivo (List<int> valori )
+        static void Esplosivo(List<int> valori)
         {
-            for (int i=  valori.Count-1;i>=0;i--)
+            for (int i = valori.Count - 1; i >= 0; i--)
             {
-              if (valori[i] == i)
-              {
+                if (valori[i] == i)
+                {
                     valori.Remove(i);
-              }
+                }
             }
             foreach (int i in valori)
             {
-                Console.WriteLine("["+i+"]");
+                Console.WriteLine("[" + i + "]");
 
             }
         }
 
         //Rotazione della Lista
-        static void Rotazione(List<int> numeri1)
+        static void Rotazione(List<int> numeri1, int scelta)
         {
-            Console.WriteLine("inserisci un numero che spostera la lista di tot valore");
-            int scelta=Convert.ToInt32 (Console.ReadLine());
 
-            for (int i = 0; i < numeri1.Count; i++)
+
+            for (int i = 0; i < scelta; i++)
             {
-                int v = 0;
-               
+                int k = 0;
+
+                k = numeri1[numeri1.Count - 1];
+                numeri1.RemoveAt(numeri1.Count - 1);
+                numeri1.Insert(0, k);
 
             }
+            
 
         }
+
+        //esercizio Picco di Frequenza Assoluta
+        static void Frequenza(List<int> numeri1)
+        {
+
+
+            
+
+
+        }
+
         static void Main(string[] args)
         {
             //indice esplosivo 
-            List<int> valori = new List<int>() { 0, 5, 2, 8, 4, 10, 6, 7 };
-            Esplosivo(valori);
-            Console.WriteLine(valori);
+            //List<int> valori = new List<int>() { 0, 5, 2, 8, 4, 10, 6, 7 };
+            //Esplosivo(valori);
+            //Console.WriteLine(valori);
 
             //Rotazione della Lista
             List<int> numeri1 = new List<int>() { 1, 2, 3, 4, 5 };
-            Rotazione(numeri1);
+            Console.WriteLine("inserisci un numero che spostera la lista di tot valore");
+            int scelta = Convert.ToInt32(Console.ReadLine());
+            Rotazione(numeri1, scelta);
+            foreach (int i in numeri1)
+            {
+               Console.WriteLine("[" + i + "]");
+
+            }
+
+            //esercizio Picco di Frequenza Assoluta
+            List<int> lista = new List<int>() { 4, 1, 4, 2, 3, 4, 4, 1, 2, 4, 9 };
         }
+       
     }
 }
